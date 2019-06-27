d78753ddd444a5c807d98e5bca73cc13c59d5ee4
Irina Bobova <irinabobova2005@gmail.com>
2019-06-27 20:55:52 +0200

    Fix

--- test/ctest.c
+++ test/ctest.c
@@ -1,44 +1,38 @@
 #include "ctest.h"
 #include "CheckWin.h"
 
-CTEST (CheckWin, correct_win_combination)
+CTEST(checkWin, correct_win_combination1)
 {
-	int board[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10,
-			11, 12}, {13, 14, 15, 0}};
-	ASSERT_EQUAL(0, CheckWin(board[][]));
+	int board[4][4] = {0};
+	ASSERT_EQUAL(0, checkWin(board));
 }
 
-CTEST(CheckWin, random_correct_combination)
+CTEST(checkWin, random_correct_combination1)
 {
-	int board[4][4] = {{13, 3, 10, 4}, {2, 14, 9, 1},
-			{15, 6, 12, 5}, {7, 11, 8, 0}};
-	ASSERT_EQUAL(0, CheckWin(board[][]));
+	int board[4][4] = {{13, 3, 10, 4}, {2, 14, 9, 1}, {15, 6, 12, 5}, {7, 11, 8, 0}};
+	ASSERT_EQUAL(0, checkWin(board));
 }
 
-CTEST(CheckWin, random_uncorrect_combination)
+CTEST(checkWin, random_uncorrect_combination2)
 {
-	int board[4][4] = {{13, 3, 10, 4}, {2, 6, 9, 1},
-			{15, 14, 12, 5}, {7, 11, 8, 0}};
-	ASSERT_EQUAL(1, CheckWin(board[][]));
+	int board[4][4] = {{13, 3, 10, 4}, {2, 6, 9, 1}, {15, 14, 12, 5}, {7, 11, 8, 0}};
+	ASSERT_EQUAL(0, checkWin(board));
 }
 
-CTEST(CheckWin, uncorrect_15_14_combination)
+CTEST(checkWin, uncorrect_15_14_combination)
 {
-	board[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10,
-			11, 12}, {13, 15, 14, 0}};
-	ASSERT_EQUAL(1, CheckWin(board[][]));
+	int board[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 15, 14, 0}};
+	ASSERT_EQUAL(0, checkWin(board));
 }
 
-CTEST(CheckWin, correct_win_combination)
+CTEST(checkWin, correct_win_combination2)
 {
-	int board[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10,
-			11, 12}, {13, 14, 15, 0}};
-	ASSERT_EQUAL(1, CheckWin(board[][]));
+	int board[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+	ASSERT_EQUAL(0, checkWin(board));
 }
 
-CTEST(CheckWin, uncorrect_win_combination)
+CTEST(checkWin, uncorrect_win_combination)
 {
-	int board[4][4] = {{2, 1, 3, 4}, {5, 6, 7, 8}, {9, 10,
-			11, 12}, {13, 14, 15, 0}};
-	ASSERT_EQUAL(0, CheckWin(board[][]));
+	int board[4][4] = {{2, 1, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+	ASSERT_EQUAL(0, checkWin(board));
 }
